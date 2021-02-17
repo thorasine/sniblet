bf39de6c9e4a322e2edde43962fb129198eedbeb
thorasine <zajakrisz@gmail.com>
2021-02-17 12:23:51 +0100

    Initial commit

---
+++ src/main/java/thorasine/sniblet/SnibletApplication.java
@@ -0,0 +1,13 @@
+package thorasine.sniblet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnibletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnibletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thorasine/sniblet/SnibletsApplicationTests.java
@@ -0,0 +1,13 @@
+package thorasine.sniblet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnibletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
